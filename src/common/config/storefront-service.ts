057ec519b59ca6363834cfc8ca073fe1ff1a93b8
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2024-12-24 12:26:29 +0530

    working on storefront service

--- src/common/config/storefront-service.ts
+++ src/common/config/storefront-service.ts
@@ -5,9 +5,27 @@ import { shopifyApi } from '@shopify/shopify-api'
 import path from 'path';
 dotenv.config({ path: path.join(__dirname, `../.env`)});
 
-export const shopify = shopifyApi({
+const shopify = shopifyApi({
     apiKey: process.env.CLIENT_ID,
     apiSecretKey: process.env.CLIENT_KEY || '',
     scopes: ["read_products"],
     hostName: process.env.HOST_NAME || 'localhost:3000',
 });
+
+const storefrontAccessToken = process.env.STOREFRONT_ACCESS_TOKEN;
+const shop = process.env.STORE_URL;
+
+// const storefrontClient = new shopify.clients.Storefront({
+//     domain: shop,
+//     storefrontAccessToken,
+// });
+
+export class StorefrontService {
+
+    constructor(private storefrontClient = new shopify.clients.Storefront({
+        domain: shop,
+        storefrontAccessToken,
+    })) {
+        
+    }
+}
