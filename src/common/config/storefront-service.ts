a1b67188969279d107f1b1a5326e83685b63170b
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2024-12-26 12:16:56 +0530

    working on to gety session token

--- src/common/config/storefront-service.ts
+++ src/common/config/storefront-service.ts
@@ -1,12 +1,26 @@
 import * as dotenv from 'dotenv';
 import '@shopify/shopify-api/adapters/node';
 // require("@shopify/shopify-api/adapters/node");
-import { ApiVersion, ConfigInterface, ConfigParams, shopifyApi, ShopifyRestResources } from '@shopify/shopify-api'
-
+import { ApiVersion, ConfigInterface, ConfigParams, shopifyApi, ShopifyRestResources } from '@shopify/shopify-api';
+import createApp from "@shopify/app-bridge";
+import { getSessionToken } from "@shopify/app-bridge/utilities";
 import path from 'path';
 // import { FutureFlagOptions } from '@shopify/shopify-api/dist/ts/future/flags';
 dotenv.config({ path: path.join(__dirname, `../../../../.env`) });
 // console.log("env var", process.env.CLIENT_KEY , path.join(__dirname, `../../../../.env`))
+
+const app = createApp({
+    apiKey: "5997eeadcc624e599895ed29528dc33b", // API key from the Partner Dashboard
+    host: "quickstart-8735ef1f.myshopify.com", // host from URL search parameter
+});
+app.getState().then((state) => { console.info('App State:', state); });
+const sessionToken = async () => await getSessionToken(app);
+// async () => await getSessionToken(app);
+// sessionToken.then((result: any)=> {
+//     console.log('sessionToken', result)
+// }).catch((error)=> {
+//     console.log('sessionToken error', error)
+// })
 
 // const shopify = shopifyApi({
 //     apiKey: process.env.CLIENT_ID,
@@ -19,6 +33,7 @@ const params = {
     apiKey: process.env.CLIENT_ID || '',
     apiSecretKey: process.env.CLIENT_KEY || '',
     accessToken: process.env.CLIENT_KEY || '',
+    billing: undefined,
     // @ts-ignore
     scopes: ["read_products"] || [],
     hostName: process.env.HOST_NAME || 'localhost:3000',
